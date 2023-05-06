320df5c879f5902406b5323c6948b16b167c8d67
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-05-06 13:31:05 +0100

    commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,35 @@
 import React from 'react'
-
+import { FaBars, FaTimes } from 'react-icons/fa'
+import Logo from '../assets/logo1.png'
 const Navbar = () => {
   return (
     <>
-      <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f]"></div>
+      <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+        <div>
+          <img src={Logo} alt="logo image" style={{ width: '50px' }} />
+        </div>
+        {/* {menu} */}
+        <div>
+          <ul className="flex">
+            <li>Home</li>
+            <li>About</li>
+            <li>Skills</li>
+            <li>Work</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        {/* {hamburger} */}
+        <div className="hidden">
+          <FaBars />
+        </div>
+        <ul className="hidden">
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Work</li>
+          <li>Contact</li>
+        </ul>
+      </div>
     </>
   )
 }
