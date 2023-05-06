978c7b71ce1a6b6cf14e3c7206f261012dae046d
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-05-06 22:07:46 +0100

    commit

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,19 +4,25 @@ const Home = () => {
   return (
     <div data-name="home" className="w-full h-screen bg-[#0a192f]">
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <p>Hi, My Name is</p>
-        <h1>Hamza Talha</h1>
-        <h2>i'm a Frontend React Developer.</h2>
-        <p>
+        <p className="text-pink-600">Hi, My Name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          Hamza Talha
+        </h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+          i'm a Frontend React Developer.
+        </h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
           hello everybody i'm trying to build my first portfolio using
           (React/Typescript) i'll do my best in this project because i really
           need to improve myself withing learning and creating websites. i'll
           deployit as soon as possible on github stay stunned!
         </p>
         <div>
-          <button>
+          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
             View Work
-            <HiArrowNarrowRight />
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-3" />
+            </span>
           </button>
         </div>
       </div>
