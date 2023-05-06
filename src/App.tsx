4ba0d819db5d7ab90b6cbdc64481dfff712948da
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-05-06 18:15:32 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Home from './components/Home'
 import Navbar from './components/Navbar'
 import './main.css'
 function App() {
   return (
     <div>
       <Navbar />
+      <Home />
     </div>
   )
 }
