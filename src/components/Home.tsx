13c6d33dd861ba2ab4bc99bdd6bd49f19e333fcc
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-05-06 20:39:54 +0100

    commit

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,27 @@
 import React from 'react'
-
+import { HiArrowNarrowRight } from 'react-icons/hi'
 const Home = () => {
-  return <div className="w-full h-screen bg-[#0a192f]">Home</div>
+  return (
+    <div data-name="home" className="w-full h-screen bg-[#0a192f]">
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p>Hi, My Name is</p>
+        <h1>Hamza Talha</h1>
+        <h2>i'm a Frontend React Developer.</h2>
+        <p>
+          hello everybody i'm trying to build my first portfolio using
+          (React/Typescript) i'll do my best in this project because i really
+          need to improve myself withing learning and creating websites. i'll
+          deployit as soon as possible on github stay stunned!
+        </p>
+        <div>
+          <button>
+            View Work
+            <HiArrowNarrowRight />
+          </button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
