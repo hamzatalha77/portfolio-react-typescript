36ec38fac0ec3ccbb8f5ede58a4a055e58bdb4ee
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-05-07 13:45:31 +0100

    commit errors

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,8 @@ import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa'
 import { HiOutlineMail } from 'react-icons/hi'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
 import Logo from '../assets/logo1.png'
+import { Link } from 'react-scroll'
+
 const Navbar = () => {
   const [nav, setNav] = useState(false)
 
@@ -17,13 +19,33 @@ const Navbar = () => {
         </div>
         {/* {menu} */}
 
-        <ul className="hidden md:flex">
-          <li>Home</li>
-          <li>About</li>
-          <li>Skills</li>
-          <li>Work</li>
-          <li>Contact</li>
-        </ul>
+        {/* <ul className="hidden md:flex">
+          <li>
+            <Link to="home" smooth={true} duration={500}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="about" smooth={true} duration={500}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="skills" smooth={true} duration={500}>
+              Skills
+            </Link>
+          </li>
+          <li>
+            <Link to="work" smooth={true} duration={500}>
+              Work
+            </Link>
+          </li>
+          <li>
+            <Link to="contact" smooth={true} duration={500}>
+              Contact
+            </Link>
+          </li>
+        </ul> */}
 
         {/* {hamburger} */}
         <div onClick={openNav} className=" md:hidden z-10">
--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,15 +1,24 @@
 import React from 'react'
-
+import WorkinImg from '../assets/workImg.jpeg'
+import realEstate from '../assets/realestate.jpg'
 const Work = () => {
   return (
-    <div className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
+    <div
+      data-name="work"
+      className="w-full md:h-screen text-gray-300 bg-[#0a192f]"
+    >
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div className="pb-8">
-          <p className="text-4xl ">Work</p>
-          <p>// Check out some of my recent work</p>
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
+            Work
+          </p>
+          <p className="py-6">// Check out some of my recent work</p>
         </div>
-        <div>
-          <div>
+        <div
+          style={{ backgroundImage: `url(${WorkinImg})` }}
+          className="grid sm:grid-cols-2 md:grid-cols-3 gap-4"
+        >
+          <div className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
             <div>
               <span></span>
               <div>
