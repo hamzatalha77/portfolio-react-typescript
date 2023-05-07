6bd01beac2382b102fbe33ccb914a9c001dfcb9b
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-05-07 19:10:21 +0100

    commit

--- src/data/data.js
+++ src/data/data.js
@@ -6,7 +6,7 @@ export const data = [
     id: 1,
     name: 'React JS Application',
     image: WorkIm,
-    github: '',
+    github: 'gh repo clone hamzatalha77/firebase-reactjs-typescript',
     live: '',
   },
   {
