5cd41565d8fbed298ef956fb8331bcfddeb657e9
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-05-18 11:36:59 +0100

    commit

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import WorkinImg from '../assets/workImg.jpeg'
-import realEstate from '../assets/realestate.jpg'
 
 const Work = () => {
   return (
@@ -13,7 +12,7 @@ const Work = () => {
           <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
             Work
           </p>
-          <p className="py-6">// Check out some of my recent work</p>
+          <p className="py-6">/Check out some of my recent work</p>
         </div>
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           <div
