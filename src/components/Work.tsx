4625618f2a53bc0b7b437b90d011e1e038f3e224
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-05-23 19:12:43 +0100

    commit

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -6,6 +6,7 @@ const firebaseConfig = {
   apiKey: 'AIzaSyBrr86OuE6dPID1YZS1klQCLQ8bWtaON_I',
   authDomain: 'something-5e33c.firebaseapp.com',
   projectId: 'something-5e33c',
+  databaseURL: 'https://something-5e33c-default-rtdb.firebaseio.com/',
   storageBucket: 'something-5e33c.appspot.com',
   messagingSenderId: '886615306959',
   appId: '1:886615306959:web:716429962ce8af37f6fdba',
