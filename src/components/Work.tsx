0e27c94f9c5602cfbede365d93d003165779be6c
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-05-24 13:59:15 +0100

    commit

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,20 +1,4 @@
 import { useEffect, useState } from 'react'
-import { initializeApp } from 'firebase/app'
-import { collection, getDocs, getFirestore } from 'firebase/firestore'
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyBrr86OuE6dPID1YZS1klQCLQ8bWtaON_I',
-  authDomain: 'something-5e33c.firebaseapp.com',
-  projectId: 'something-5e33c',
-  databaseURL: 'https://something-5e33c-default-rtdb.firebaseio.com/',
-  storageBucket: 'something-5e33c.appspot.com',
-  messagingSenderId: '886615306959',
-  appId: '1:886615306959:web:716429962ce8af37f6fdba',
-}
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig)
-const firestore = getFirestore(app)
 
 const Work = () => {
   const [data, setData] = useState<any[]>([])
--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,8 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
-// import { getDatabase } from 'firebase/database'
-import { getFirestore } from 'firebase/firestore'
-import { getStorage } from 'firebase/storage'
+import { collection, getDocs, getFirestore } from 'firebase/firestore'
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -11,12 +10,12 @@ const firebaseConfig = {
   apiKey: 'AIzaSyBrr86OuE6dPID1YZS1klQCLQ8bWtaON_I',
   authDomain: 'something-5e33c.firebaseapp.com',
   projectId: 'something-5e33c',
+  databaseURL: 'https://something-5e33c-default-rtdb.firebaseio.com/',
   storageBucket: 'something-5e33c.appspot.com',
   messagingSenderId: '886615306959',
   appId: '1:886615306959:web:716429962ce8af37f6fdba',
 }
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig)
-export const storage = getStorage(app)
-export const db = getFirestore(app)
+export const app = initializeApp(firebaseConfig)
+export const firestore = getFirestore(app)
