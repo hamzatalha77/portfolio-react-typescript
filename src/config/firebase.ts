42ed37def66d375a9f049a4adac4c704e363852e
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-05-24 19:12:00 +0100

    commit

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -7,13 +7,12 @@ import { getFirestore } from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  databaseURL: process.env.DATABASE_URL,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
+  apiKey: 'AIzaSyBrr86OuE6dPID1YZS1klQCLQ8bWtaON_I',
+  authDomain: 'something-5e33c.firebaseapp.com',
+  projectId: 'something-5e33c',
+  storageBucket: 'something-5e33c.appspot.com',
+  messagingSenderId: '886615306959',
+  appId: '1:886615306959:web:716429962ce8af37f6fdba',
 }
 
 // Initialize Firebase
