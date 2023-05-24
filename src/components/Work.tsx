1b5e15744d9dadbd4acd5b0dafe20ae91bd5439a
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-05-24 12:37:10 +0100

    commit

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -55,7 +55,7 @@ const Work = () => {
               className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
             >
               <div className="opacity-0 group-hover:opacity-100">
-                <span className="text-2xl font-bold text-white tracking-wide">
+                <span className="text-2xl font bold text-white tracking-wider ">
                   {item.name}
                 </span>
                 <div className="pt-8 text-center">
