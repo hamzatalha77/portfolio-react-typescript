896a42b5fd4f7b723ea96929214e7a02508d9aff
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-05-24 13:55:45 +0100

    commit

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -12,7 +12,7 @@ const Contact = () => {
             Contact
           </p>
           <p className="text-gray-300 py-4">
-            // Submit the form below or shoot me an email -
+            / Submit the form below or shoot me an email -
             hamzatalha.web7@gmail.com
           </p>
         </div>
