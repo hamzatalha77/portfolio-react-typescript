d557cb7cdc4f888d4b3612f420bbe3ca39c23ebb
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-05-27 18:04:07 +0100

    commit

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -24,7 +24,7 @@ const Work = () => {
   return (
     <div
       id="work"
-      className="w-full md:h-screen  dark:bg-[#F2F3F4] text-gray-300 bg-[#121212]"
+      className="w-full md:h-screen  dark:bg-[#eaeaea] text-gray-300 bg-[#121212]"
     >
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div className="pb-8">
