2c0980ffbd127927b405a3900c29f9125d61da08
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-05-27 13:19:07 +0100

    COMMIT

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -23,7 +23,7 @@ const Navbar = () => {
 
   return (
     <>
-      <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+      <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] dark:bg-[#000] text-gray-300">
         <div>
           <img src={Logo} alt="logoimage" style={{ width: '50px' }} />
           <button
