19761d7608dbf3d6a7d69db6ad0397a1a734030a
hamzatalha77 <hamza_ta7@hotmail.com>
2023-09-05 12:12:43 +0100

    commit

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -6,7 +6,7 @@ const Contact = () => {
     >
       <form action="POST" className="flex flex-col max-w-[600px] w-full">
         <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-[#fa5923]">
             Contact
           </p>
           <p className="text-gray-300 py-4">
@@ -31,7 +31,7 @@ const Contact = () => {
           className="bg-[#ccd6f6] p-2"
           rows={10}
         ></textarea>
-        <button className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center">
+        <button className="text-white border-2 hover:bg-pink-600 hover:border-[#fa5923] px-4 py-3 my-8 mx-auto flex items-center">
           Let's collabe
         </button>
       </form>
