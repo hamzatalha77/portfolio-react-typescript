becd609a9a87fa550b0145a6ad0b72d776dcb846
hamzatalha77 <hamza_ta7@hotmail.com>
2023-09-07 12:33:33 +0100

    commit

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div id="home" className="w-full h-screen bg-[#fffdf9] dark:bg-[#191a19]">
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <p className="text-[#b22725]">Hi, My Name is</p>
+        <p className="text-[#b22725] text-2xl">Hi 👋, My Name is</p>
         <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
           Hamza Talha
         </h1>
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -11,7 +11,7 @@ const Skills = () => {
   return (
     <div
       id="skill"
-      className="w-full h-screen bg-[#fffdf9]  dark:bg-[#191a19] text-[#444544] dark:text-[#b22725]"
+      className="w-full h-screen bg-[#fffdf9]  dark:bg-[#191a19] text-[#b22725] dark:text-[#444544]"
     >
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
