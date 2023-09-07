59e147c328e3cb6d6934d019b9a02b2fe8648808
hamzatalha77 <hamza_ta7@hotmail.com>
2023-09-07 12:15:47 +0100

    commit

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,19 +2,19 @@ const About = () => {
   return (
     <div
       id="about"
-      className="w-full h-screen bg-[#fffdf9]  dark:bg-[#191a19] text-[#444544]"
+      className="w-full h-screen bg-[#fffdf9]  dark:bg-[#191a19] text-[#b22725] dark:text-[#444544]"
     >
       <div className="flex flex-col justify-center items-center w-full h-full">
         <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
           <div className="sm:text-right pb-8 pl-4">
-            <p className="text-4xl font-bold inline border-b-4 border-[#b22725]">
+            <p className="text-4xl font-bold inline border-b-4 border-[#444544]  dark:border-[#b22725] ">
               About
             </p>
           </div>
           <div></div>
         </div>
         <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
-          <div className="sm:text-right text-4xl font-bold text-[#ccd6f6]">
+          <div className="sm:text-right text-4xl font-bold text-[#8892b0] dark:text-[#ccd6f6]">
             <p>Hi. i'm Hamza, nice to see you here please take a sit. </p>
           </div>
           <div>
