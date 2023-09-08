44ed3963aad995cedc305167d1bb278297d15fb0
hamzatalha77 <hamza_ta7@hotmail.com>
2023-09-08 19:54:02 +0100

    commit

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -41,21 +41,22 @@ const Work = () => {
             <div
               key={item.id}
               style={{ backgroundImage: `url(${item.imageUrl})` }}
-              className="shadow-lg shadow-[#000000] group container rounded-md 
-              flex justify-center text-center items-center mx-auto content-div "
+              className="shadow-lg shadow-[#000000] group container rounded-md flex justify-center text-center items-center mx-auto content-div scroll-on-hover"
             >
-              <div className="opacity-0 group-hover:opacity-100 ">
-                <span className="text-2xl font bold text-white tracking-wider ">
-                  {item.name}
-                </span>
-                <div className="pt-8 text-center">
-                  <a href={item.github}>
-                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg flex">
-                      <FaGithub size={25} className="mr-2" />
-                      GitHub
-                    </button>
-                  </a>
-                  <p className="text-left text-sm text-white">{item.live}</p>
+              <div className="content-container">
+                <div className="opacity-0 group-hover:opacity-100 ">
+                  <span className="text-2xl font bold text-white tracking-wider">
+                    {item.name}
+                  </span>
+                  <div className="pt-8 text-center">
+                    <a href={item.github}>
+                      <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg flex">
+                        <FaGithub size={25} className="mr-2" />
+                        GitHub
+                      </button>
+                    </a>
+                    <p className="text-left text-sm text-white">{item.live}</p>
+                  </div>
                 </div>
               </div>
             </div>
