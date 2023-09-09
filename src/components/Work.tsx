c1db2ddd28a461bc1d4fab27ed87363d07cecb90
hamzatalha77 <hamza_ta7@hotmail.com>
2023-09-09 12:58:34 +0100

    commit

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -36,21 +36,37 @@ const Work = () => {
             ✍ /Check out some of my recent work
           </p>
         </div>
-        <div className="grid-cols-1 sm:grid md:grid-cols-3">
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           {data.map((item) => (
-            <div key={item.id} className="mx-3 mt-6  flex flex-col rounded-lg">
-              <div className="screen">
-                <img
-                  className="rounded-t-lg"
-                  src={item.imageUrl}
-                  alt="Hollywood Sign on The Hill"
-                />
-              </div>
-              {/* <div className="p-6">
-                <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+            <div
+              key={item.id}
+              style={{ backgroundImage: `url(${item.imageUrl})` }}
+              className="shadow-lg shadow-[#040c16] group container rounded-md 
+              flex justify-center text-center items-center mx-auto content-div "
+            >
+              <div className="opacity-0 group-hover:opacity-100 ">
+                <span className="text-2xl font bold text-white tracking-wider ">
                   {item.name}
-                </h5>
-              </div> */}
+                </span>
+                <div className="pt-8 text-center ">
+                  <a href={item.github} target="blank">
+                    <button
+                      className="text-center rounded-lg px-4 py-3 m-2
+                       bg-white text-gray-700 font-bold text-lg"
+                    >
+                      Code
+                    </button>
+                  </a>
+                  <a href={item.live} target="blank">
+                    <button
+                      className="text-center rounded-lg px-4 py-3 m-2
+                       bg-white text-gray-700 font-bold text-lg"
+                    >
+                      Live
+                    </button>
+                  </a>
+                </div>
+              </div>
             </div>
           ))}
         </div>
@@ -59,7 +75,8 @@ const Work = () => {
             <div
               key={item.id}
               style={{ backgroundImage: `url(${item.imageUrl})` }}
-              className="shadow-lg shadow-[#000000] group container rounded-md flex justify-center text-center items-center mx-auto content-div"
+              className="shadow-lg shadow-[#040c16] group container rounded-md 
+              flex justify-center text-center items-center mx-auto content-div "
             >
               <div className="opacity-0 group-hover:opacity-100 ">
                 <span className="text-2xl font bold text-white tracking-wider">
