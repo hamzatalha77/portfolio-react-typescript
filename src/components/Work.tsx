dcc46a01f6618365d2f98c9aedb621f37bf0e5c3
hamzatalha77 <hamza_ta7@hotmail.com>
2023-09-09 11:28:51 +0100

    COMMIT

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -36,12 +36,9 @@ const Work = () => {
             ✍ /Check out some of my recent work
           </p>
         </div>
-        <div className="grid-cols-1 sm:grid md:grid-cols-3 ">
+        <div className="grid-cols-1 sm:grid md:grid-cols-3">
           {data.map((item) => (
-            <div
-              key={item.id}
-              className="mx-3 mt-6  flex flex-col rounded-lg  bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 sm:shrink-0 sm:grow sm:basis-0"
-            >
+            <div key={item.id} className="mx-3 mt-6  flex flex-col rounded-lg">
               <div className="screen">
                 <img
                   className="rounded-t-lg"
@@ -49,11 +46,11 @@ const Work = () => {
                   alt="Hollywood Sign on The Hill"
                 />
               </div>
-              <div className="p-6">
+              {/* <div className="p-6">
                 <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
                   {item.name}
                 </h5>
-              </div>
+              </div> */}
             </div>
           ))}
         </div>
