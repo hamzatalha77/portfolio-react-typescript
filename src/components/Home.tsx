4ba192cdbce1becfc63f993f8dfaa6c247639020
hamzatalha77 <hamza_ta7@hotmail.com>
2023-09-11 11:46:02 +0100

    commit

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -19,11 +19,10 @@ const Home = () => {
   }, [])
   return (
     <div id="home" className="w-full h-screen bg-[#fffdf9] dark:bg-[#191a19]">
-      <div
-        className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full"
-        ref={textRef}
-      >
-        <p className="text-[#b22725] text-2xl">Hi 👋, My Name is</p>
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-[#b22725] text-2xl" ref={textRef}>
+          Hi 👋, My Name is
+        </p>
         <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
           Hamza Talha
         </h1>
