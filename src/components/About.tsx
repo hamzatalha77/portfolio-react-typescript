cc32d82a62270061cd68e730790b925bf4f8af4b
hamzatalha77 <hamza_ta7@hotmail.com>
2023-09-12 20:21:45 +0100

    COMMIT

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,10 +1,21 @@
+import { motion } from 'framer-motion'
+
 const About = () => {
   return (
     <div
       id="about"
       className="w-full h-screen bg-[#fffdf9]  dark:bg-[#191a19] text-[#b22725] dark:text-[#444544]"
     >
-      <div className="flex flex-col justify-center items-center w-full h-full">
+      <motion.div
+        variants={{
+          hidden: { opacity: 0, y: 75 },
+          visible: { opacity: 1, y: 0 }
+        }}
+        initial="hidden"
+        animate="visible"
+        transition={{ duration: 0.5, delay: 0.25 }}
+        className="flex flex-col justify-center items-center w-full h-full"
+      >
         <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
           <div className="sm:text-right pb-8 pl-4">
             <p className="text-4xl font-bold inline border-b-4 border-[#444544]  dark:border-[#b22725] ">
@@ -27,7 +38,7 @@ const About = () => {
             </p>
           </div>
         </div>
-      </div>
+      </motion.div>
     </div>
   )
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,86 +1,42 @@
 import { HiArrowNarrowRight } from 'react-icons/hi'
 import { Link } from 'react-scroll'
+import { motion, useInView, useAnimation, useIsPresent } from 'framer-motion'
 import { useRef, useEffect } from 'react'
-import { animate, stagger } from 'motion'
-import SplitType from 'split-type'
 
 const Home = () => {
-  const paragraphRef = useRef(null)
-  const hiRef = useRef(null)
-  const nameRef = useRef(null)
-  const titleRef = useRef(null)
-  const buttonRef = useRef(null)
+  const ref = useRef(null)
+  const isInView = useInView(ref, { once: true })
 
   useEffect(() => {
-    if (paragraphRef.current) {
-      animate(
-        paragraphRef.current,
-        { y: [24, 0], opacity: [0, 1] },
-        { duration: 0.8, delay: stagger(0.05) }
-      )
-    }
-    if (hiRef.current) {
-      animate(
-        hiRef.current,
-        { y: [24, 0], opacity: [0, 1] },
-        { duration: 1, delay: stagger(0.1) }
-      )
-    }
-    if (nameRef.current) {
-      animate(
-        nameRef.current,
-        { y: [24, 0], opacity: [0, 1] },
-        { duration: 1.5, delay: stagger(0.2) }
-      )
-    }
-    if (titleRef.current) {
-      animate(
-        titleRef.current,
-        { y: [24, 0], opacity: [0, 1] },
-        { duration: 2, delay: stagger(0.3) }
-      )
-    }
-    if (buttonRef.current) {
-      animate(
-        buttonRef.current,
-        { y: [24, 0], opacity: [0, 1] },
-        { duration: 2.5, delay: stagger(0.4) }
-      )
-    }
-  }, [])
-
+    console.log(isInView)
+  }, [isInView])
   return (
     <div id="home" className="w-full h-screen bg-[#fffdf9] dark:bg-[#191a19]">
-      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <p className="text-[#b22725] text-2xl" ref={hiRef}>
-          Hi 👋, My Name is
-        </p>
-        <h1
-          className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]"
-          ref={nameRef}
-        >
+      <motion.div
+        variants={{
+          hidden: { opacity: 0, y: 75 },
+          visible: { opacity: 1, y: 0 }
+        }}
+        initial="hidden"
+        animate="visible"
+        transition={{ duration: 0.5, delay: 0.25 }}
+        className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full"
+      >
+        <p className="text-[#b22725] text-2xl">Hi 👋, My Name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
           Hamza Talha
         </h1>
-        <h2
-          className="text-4xl sm:text-7xl font-bold text-[#8892b0]"
-          ref={titleRef}
-        >
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
           i'm a Software Engineer.
         </h2>
-        <p
-          className="text-[#000000] dark:text-[#f2f2f2] py-4 max-w-[700px]"
-          ref={paragraphRef}
-        >
+        <p className="text-[#000000] dark:text-[#f2f2f2] py-4 max-w-[700px]">
           hello everybody i'm trying to build my first portfolio using
           (React/Typescript) i'll do my best in this project because i really
           need to improve myself within learning and creating websites. i'll
           deploy it as soon as possible on github stay stunned!
         </p>
         <div>
-          <button
-            className="text-[#b22725] dark:text-[#ffffff] group border-2 px-6 py-3 my-2 flex items-center dark:border-[#b22725] dark:bg-[#b22725] hover:bg-[#191a19] hover:border-[#191a19] dark:hover:bg-transparent border-[#b22725] dark:hover:border-[#b22725]"
-            ref={buttonRef}
-          >
+          <button className="text-[#b22725] dark:text-[#ffffff] group border-2 px-6 py-3 my-2 flex items-center dark:border-[#b22725] dark:bg-[#b22725] hover:bg-[#191a19] hover:border-[#191a19] dark:hover:bg-transparent border-[#b22725] dark:hover:border-[#b22725]">
             <Link to="work" smooth={true} duration={500}>
               View Work
             </Link>
@@ -89,7 +45,7 @@ const Home = () => {
             </span>
           </button>
         </div>
-      </div>
+      </motion.div>
     </div>
   )
 }
