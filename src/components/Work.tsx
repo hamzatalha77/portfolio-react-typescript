dba0185d3d40ea36a126a3ce467844487a6f4f27
hamzatalha77 <hamza_ta7@hotmail.com>
2023-09-13 12:41:58 +0100

    COMMIT

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -2,6 +2,7 @@ import { collection, getDocs } from 'firebase/firestore'
 import { useEffect, useState } from 'react'
 import { firestore } from '../config/firebase'
 import { FaGithub } from 'react-icons/fa'
+import Reveal from '../typescript/Reveal'
 
 const Work = () => {
   const [data, setData] = useState<any[]>([])
@@ -29,37 +30,44 @@ const Work = () => {
     >
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-[#444544] dark:border-[#b22725]">
-            Work
-          </p>
-          <p className="py-6 text-[#000000] dark:text-[#f2f2f2]">
-            ✍ /Check out some of my recent work
-          </p>
+          <Reveal>
+            <p className="text-4xl font-bold inline border-b-4 border-[#444544] dark:border-[#b22725]">
+              Work
+            </p>
+          </Reveal>
+
+          <Reveal>
+            <p className="py-6 text-[#000000] dark:text-[#f2f2f2]">
+              ✍ /Check out some of my recent work
+            </p>
+          </Reveal>
         </div>
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           {data.map((item) => (
-            <div
-              key={item.id}
-              style={{ backgroundImage: `url(${item.imageUrl})` }}
-              className="shadow-lg shadow-[#040c16] group container rounded-md 
+            <Reveal>
+              <div
+                key={item.id}
+                style={{ backgroundImage: `url(${item.imageUrl})` }}
+                className="shadow-lg shadow-[#040c16] group container rounded-md 
               flex justify-center text-center items-center mx-auto content-div "
-            >
-              <div className="opacity-0 group-hover:opacity-100 group-hover:w-1/2 group-hover:transition-all  group-hover:duration-1000">
-                <span className="text-2xl font bold text-[#b22725] tracking-wider ">
-                  {item.name}
-                </span>
-                <div className="pt-8 text-center ">
-                  <a href={item.github} target="blank">
-                    <button
-                      className="text-center rounded-lg px-4 py-3 m-2
+              >
+                <div className="opacity-0 group-hover:opacity-100 group-hover:w-1/2 group-hover:transition-all  group-hover:duration-1000">
+                  <span className="text-2xl font bold text-[#b22725] tracking-wider ">
+                    {item.name}
+                  </span>
+                  <div className="pt-8 text-center ">
+                    <a href={item.github} target="blank">
+                      <button
+                        className="text-center rounded-lg px-4 py-3 m-2
                        bg-white text-[#b22725] font-bold text-lg"
-                    >
-                      <FaGithub size={30} />
-                    </button>
-                  </a>
+                      >
+                        <FaGithub size={30} />
+                      </button>
+                    </a>
+                  </div>
                 </div>
               </div>
-            </div>
+            </Reveal>
           ))}
         </div>
       </div>
