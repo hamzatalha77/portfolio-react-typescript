dfdcaca8f9e202306f05b4afb151c6ea6ce4db7e
hamzatalha77 <hamza_ta7@hotmail.com>
2023-09-13 12:27:30 +0100

    comit

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,34 +1,10 @@
-import { motion, useInView, useAnimation } from 'framer-motion'
-import { useRef, useEffect } from 'react'
 const About = () => {
-  const ref = useRef(null)
-  const isInView = useInView(ref, { once: true })
-
-  const mainControls = useAnimation()
-  const slideControls = useAnimation()
-
-  useEffect(() => {
-    if (isInView) {
-      mainControls.start('visible')
-      slideControls.start('visible')
-    }
-  }, [isInView, mainControls, slideControls])
   return (
     <div
-      ref={ref}
       id="about"
       className="w-full h-screen bg-[#fffdf9]  dark:bg-[#191a19] text-[#b22725] dark:text-[#444544]"
     >
-      <motion.div
-        variants={{
-          hidden: { opacity: 0, y: 75 },
-          visible: { opacity: 1, y: 0 }
-        }}
-        initial="hidden"
-        animate={mainControls}
-        transition={{ duration: 1.5, delay: 1 }}
-        className="flex flex-col justify-center items-center w-full h-full"
-      >
+      <div className="flex flex-col justify-center items-center w-full h-full">
         <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
           <div className="sm:text-right pb-8 pl-4">
             <p className="text-4xl font-bold inline border-b-4 border-[#444544]  dark:border-[#b22725] ">
@@ -51,7 +27,7 @@ const About = () => {
             </p>
           </div>
         </div>
-      </motion.div>
+      </div>
     </div>
   )
 }
