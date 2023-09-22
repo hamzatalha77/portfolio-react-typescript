e86d2d8e17b7bb24b28f82bfc80ff07629046cd8
hamzatalha77 <hamza_ta7@hotmail.com>
2023-09-22 13:09:32 +0100

    commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -160,7 +160,7 @@ const Navbar = () => {
             <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#ff7e67]">
               <a
                 className="flex justify-between items-center w-full text-[#f1f1f1]"
-                href="https://firebasestorage.googleapis.com/v0/b/something-5e33c.appspot.com/o/Hamza%20Talha%20CV%202023.pdf?alt=media&token=81c9742c-c210-4832-a717-fb87c782ba11"
+                href="https://firebasestorage.googleapis.com/v0/b/something-5e33c.appspot.com/o/Hamza%20Talha%20CV%202023.pdf?alt=media&token=ec9cf985-3c6a-42b0-80c0-f25015694ea0"
               >
                 Resume <BsFillPersonLinesFill size={30} />
               </a>
